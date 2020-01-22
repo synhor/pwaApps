94da966696f0d29aa746dc8fd1ab4ff547121a37
akruss <akruss@esri.pl>
2020-01-22 09:40:52 +0100

    Krok 5: Opcjonalne  wyłączenie

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 const appPath = location.href.replace('/sw.js','');
 const expectedCache = 'appOne_v2';
+const useServiceWorker = true;
+const serviceWorkerExcludedHosts = [];
 
 const filesToCache = [
     appPath + '/images/',
@@ -8,33 +10,44 @@ const filesToCache = [
     appPath + '/style.css'
 ];
 
-// Krok 2.1: stworzenie nowego cache’a o nazwie v1
-// W tym celu dodamy event listenter na zdarzenie fetch.
-self.addEventListener('install', event => {
-    console.log(expectedCache + ' installing…');
-    event.waitUntil(caches.open(expectedCache).then(cache => {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(filesToCache);
-    }));
-});
-  
-//Krok 3: teraz aby móc korzystać z scachowanych zasobów musimy jeszcze zwrócić je do lokalnego cache’a.
-//W tym celu dodamy event listenter na zdarzenie fetch.
+if (!useServiceWorker || serviceWorkerExcludedHosts.includes(self.registration.scope)) {
+    //Krok 5: Opcjonalne wyłączanie
+    self.addEventListener('install', event => {
+        console.log('[ServiceWorker] Servise worker will be disabled');
+        event.waitUntil(caches.open(expectedCache).then(cache => {
+            console.log('[ServiceWorker] Servise worker is disabled');
+            return self.registration.unregister();
+        }));
+    });
+} else {
+    // Krok 2.1: stworzenie nowego cache’a o nazwie v1
+    // W tym celu dodamy event listenter na zdarzenie fetch.
+    self.addEventListener('install', event => {
+        console.log(expectedCache + ' installing…');
+        event.waitUntil(caches.open(expectedCache).then(cache => {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        }));
+    });
+    
+    //Krok 3: teraz aby móc korzystać z scachowanych zasobów musimy jeszcze zwrócić je do lokalnego cache’a.
+    //W tym celu dodamy event listenter na zdarzenie fetch.
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.open(expectedCache).then(cache =>
-        cache.match(event.request).then(response => {
-            if (response) {
-                console.log('[ServiceWorker] loaded from cache: ', event.request.url);
-            }
-            return response || fetch(event.request).then(response => {
-                cache.put(event.request, response.clone());
-                console.log('[ServiceWorker] added to cache: ', event.request.url);
-                return response;
+    self.addEventListener('fetch', event => {
+        event.respondWith(caches.open(expectedCache).then(cache =>
+            cache.match(event.request).then(response => {
+                if (response) {
+                    console.log('[ServiceWorker] loaded from cache: ', event.request.url);
+                }
+                return response || fetch(event.request).then(response => {
+                    cache.put(event.request, response.clone());
+                    console.log('[ServiceWorker] added to cache: ', event.request.url);
+                    return response;
+                })
             })
-        })
-    )); 
-});
+        )); 
+    });
+}
 
 //Krok 4: kontrola wersji i odswieżanie
 //W tym celu dodamy event listenter na zdarzenie fetch.
@@ -49,5 +62,5 @@ self.addEventListener('activate', event => {
         })
     )).then(() => {
         console.log('[ServiceWorker] Version ' + expectedCache + ' now ready to handle fetches!');
-    }));
+    }));    
 });
