f0e242f1dd63b9834cdbe84534b34cb49327af5b
akruss <akruss@esri.pl>
2020-01-22 09:25:19 +0100

    Krok 3: wypełnienie lokalnego cache na żądanie

--- sw.js
+++ sw.js
@@ -16,3 +16,20 @@ self.addEventListener('install', event => {
     }));
 });
   
+//Krok 3: teraz aby móc korzystać z scachowanych zasobów musimy jeszcze zwrócić je do lokalnego cache’a.
+//W tym celu dodamy event listenter na zdarzenie fetch.
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.open('v1').then(cache =>
+        cache.match(event.request).then(response => {
+            if (response) {
+                console.log('[ServiceWorker] loaded from cache: ', event.request.url);
+            }
+            return response || fetch(event.request).then(response => {
+                cache.put(event.request, response.clone());
+                console.log('[ServiceWorker] added to cache: ', event.request.url);
+                return response;
+            })
+        })
+    )); 
+});
