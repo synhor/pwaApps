fad8d6b34fe065d63a30362c1a76253480ae547b
akruss <akruss@esri.pl>
2020-01-22 09:35:22 +0100

    Krok 4: kontrola wersji cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const appPath = location.href.replace('/sw.js','');
+const expectedCache = 'appOne_v2';
 
 const filesToCache = [
     appPath + '/images/',
@@ -8,9 +9,10 @@ const filesToCache = [
 ];
 
 // Krok 2.1: stworzenie nowego cache’a o nazwie v1
+// W tym celu dodamy event listenter na zdarzenie fetch.
 self.addEventListener('install', event => {
-    console.log('V1 installing…');
-    event.waitUntil(caches.open('v1').then(cache => {
+    console.log(expectedCache + ' installing…');
+    event.waitUntil(caches.open(expectedCache).then(cache => {
         console.log('[ServiceWorker] Caching app shell');
         return cache.addAll(filesToCache);
     }));
@@ -20,7 +22,7 @@ self.addEventListener('install', event => {
 //W tym celu dodamy event listenter na zdarzenie fetch.
 
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.open('v1').then(cache =>
+    event.respondWith(caches.open(expectedCache).then(cache =>
         cache.match(event.request).then(response => {
             if (response) {
                 console.log('[ServiceWorker] loaded from cache: ', event.request.url);
@@ -32,4 +34,20 @@ self.addEventListener('fetch', event => {
             })
         })
     )); 
+});
+
+//Krok 4: kontrola wersji i odswieżanie
+//W tym celu dodamy event listenter na zdarzenie fetch.
+self.addEventListener('activate', event => {
+    event.waitUntil(caches.keys().then(keys => 
+        Promise.all(keys.map(key => {
+            if (expectedCache !== key) {
+                console.log('[ServiceWorker] New cache version detected:' + expectedCache);
+                console.log('[ServiceWorker] Deleting old cache:' + key);
+                return caches.delete(key);
+            }
+        })
+    )).then(() => {
+        console.log('[ServiceWorker] Version ' + expectedCache + ' now ready to handle fetches!');
+    }));
 });
